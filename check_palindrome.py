23ca4c7891de84e23c0fd2b80057dbbab1bc95ab
rahulinux <loginrahul90@gmail.com>
2016-07-03 20:51:40 +0530

    practice of the day palindrom

--- check_palindrome.py
+++ check_palindrome.py
@@ -6,12 +6,13 @@ A palindrome is a word, phrase, number or other sequence of units that has the p
 
 ### Pythonic way using data structure 
 #def is_palindrom(data):
-#    return str(data)[::-1] == str(data)
+#    data = str(data).lower()
+#    return data[::-1] == data
 
 
 ### Non pythonic way but using algoritham
 def is_palindrom(data):
-   data = str(data)
+   data = str(data).lower()
    data_length = len(data)
    i = 0
    while i <= data_length/2:
@@ -23,4 +24,5 @@ def is_palindrom(data):
 
 print is_palindrom(11)
 print is_palindrom("rahul")
-print is_palindrom("ana")
+print is_palindrom("Ana")
+print is_palindrom("a")
